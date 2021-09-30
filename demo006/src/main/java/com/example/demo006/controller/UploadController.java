75ae6e2212932fa14a7ce489fb9dda0e4711c1b5
ZYHZ <Yu-Hang.Zhang@outlook.com>
2021-09-30 13:08:40 +0800

    完善demo006的说明文档

--- demo006/src/main/java/com/example/demo006/controller/UploadController.java
+++ demo006/src/main/java/com/example/demo006/controller/UploadController.java
@@ -2,13 +2,12 @@ package com.example.demo006.controller;
 
 import com.example.demo006.service.UploadService;
 import com.example.demo006.utils.WebResponse;
-import lombok.Value;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author by zhangyuhang
@@ -32,11 +31,11 @@ public class UploadController {
     }
 
     @RequestMapping("/excel")
-    public WebResponse uploadExcel(MultipartFile file){
-        if (file == null){
+    public WebResponse uploadExcel(MultipartFile excel){
+        if (excel == null){
             return WebResponse.ofError("上传文件为空");
         }
-        return uploadService.saveExcel(file);
+        return uploadService.saveExcel(excel);
     }
 
 }
